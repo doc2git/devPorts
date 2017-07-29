a2f88c5c75f8cbaf43bafc45b736fc429ad4b31e
2838309423 <2838309423@qq.com>
2017-07-30 05:06:44 +0800

    1st

---
+++ demo_server.js
@@ -0,0 +1,3 @@
+/** let count = 0; setInterval(()=>{ count++ % 2 ? console.log("what is that ?") : console.log("How to ？");  }, 100); */
+let genPort = require('./pServer');
+console.log(genPort(4003, {4442: 8723, a: 8832}, [4222, 8797]));;
